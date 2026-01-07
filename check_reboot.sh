dd858b8a2c139f51cad5d030078738ffd99993a3
Jose Ferreira <jj.ferreira@gmail.com>
2026-01-07 15:15:50 -0600

    reboot script used for MB's RHEL7/8 weekly reboots

---
+++ check_reboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This scripts checks if a reboot is required after weekly update installation using dnf-automatic on RHEL 7/8 servers
+# since dnf-automatic auto reboot option is not available on pre RHEL 9 servers.
+
+/bin/needs-restarting -r | grep -i "reboot should not be necessary"
+
+if [ $? != 0 ]
+then
+	echo "Reboot is required following weekly patching.  Server is rebooting now!"
+	reboot
+else
+	echo "No reboot is required."
+fi
+
